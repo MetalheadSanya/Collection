e1fa670c9c6e252c350a59317934f896f6f51481
Alexander Zalutskiy <metalhead.sanya@gmail.com>
2016-12-28 22:45:46 +0300

    Initial Commit

---
+++ Collection/Collection.h
@@ -0,0 +1,19 @@
+//
+//  Collection.h
+//  Collection
+//
+//  Created by Alexander Zalutskiy on 28.12.16.
+//  Copyright © 2016 Alexander Zalutskiy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Collection.
+FOUNDATION_EXPORT double CollectionVersionNumber;
+
+//! Project version string for Collection.
+FOUNDATION_EXPORT const unsigned char CollectionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Collection/PublicHeader.h>
+
+
